b595af2a9b1128cc7a5537675ada76d2ca019d35
cristian mosqueda <cmosqued@gmail.com>
2020-10-01 22:44:19 -0700

    created AddingMachine.java

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,58 @@
+package cse360assignment02;
+
+
+/**
+ * Adding Machine class for Assignment 01 in CSE 360
+ * @author cristianmosqueda
+ */
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Constructor for AddingMachine class
+     */
+    public AddingMachine() {
+        total = 0;
+    }
+
+    /**
+     *
+     * @return total value
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     *
+     * @param value
+     */
+    public void add(int value) {
+
+    }
+
+    /**
+     *
+     * @param value
+     */
+    public void subtract(int value) {
+
+    }
+
+    /**
+     *
+     * @return a string
+     */
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    /**
+     *
+     */
+    public void clear() {
+
+    }
+
+}
